bffe7f10f618a2614f0dadfb08c277c230f19b79
Anishbarveen <anisasmina2910@gmail.com>
2017-09-12 16:55:32 +0530

    fifth commit

--- LetsChatBE/src/main/java/com/LetsChatBE/model/Users.java
+++ LetsChatBE/src/main/java/com/LetsChatBE/model/Users.java
@@ -4,8 +4,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import org.springframework.stereotype.Component;
 
@@ -18,12 +21,18 @@ public class Users {
 	
 	@Id
 	private  String username;
-	@Column
+	@Size(min=1,max=12,message="Invalid first name")
 	private String firstname;
+	@Column(name="EMAIL", length=50)
+	@NotNull
+	@Size(min=7,max=50)
+	@Pattern(regexp="^.+@.+\\..+$")
 	private String email;
-	@NotNull
+
+	@Size(min=1,max=12,message="Invalid first name")
 	private String lastname;
-	@NotNull
+	
+	@Size(min=1,max=10)
 	private String password;
 	
 	private String role;
